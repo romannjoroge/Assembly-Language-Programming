94549ceff4edacf247fb97e9d0b28568222a0840
romannjoroge <roman.njoroge@njuguna.com>
2023-05-15 19:26:43 +0300

    feat: Using C functions in assembly

--- c_example.asm
+++ c_example.asm
@@ -1,14 +1,28 @@
-section .text
 global main
 extern printf
 
 section .data
     msg db "Testing %i...", 0xa, 0x0 ; 0x0 indicates the end of the string
+    debug_info db '1'
+    space db 0xa
 
 main:
     ; prolog of function
     push ebp
     mov ebp, esp
+
+    ; Printing debug info
+    mov eax, 4
+    mov ebx, 1
+    mov edx, 1
+    mov ecx, debug_info
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov edx, 1
+    mov ecx, space
+    int 0x80
 
     ; push arguements for printf
     push 123
@@ -21,4 +35,7 @@ main:
 
     mov esp, ebp
     pop ebp
-    ret
+    
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
